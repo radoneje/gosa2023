06330a6239b1e82d5f780002e8062630d87be6e3
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-17 02:22:40 +0300

    Initial dcommit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/:lang?', function(req, res, next) {
   let lang =req.params.lang;
-  if(! (lang && lang.match(/$(ru|en)^/)))
+  if(! (lang && lang.match(/ru|en/)))
     res.redirect("/gosa2023/ru")
   res.render('index', { lang });
 });
