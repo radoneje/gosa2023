b1d2cca0ec21915626f4eadbcb1547cf52d2a0f0
radoneje <53825282+radoneje@users.noreply.github.com>
2023-04-17 02:11:02 +0300

    Initial dcommit

---
+++ routes/indexRouter.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/:lang?', function(req, res, next) {
+  let lang =req.params.lang;
+  if(! (lang && lang.match(/$(ru|en)^/)))
+    res.redirect("ru")
+  res.render('index', { lang });
+});
+
+module.exports = router;
